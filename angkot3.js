68c173fa50a73046bdafa6a85b8921389d1b2674
fedrotogatorop <fedrotogatorop>
2024-09-11 17:03:00 +0700

    menambahkan angkot3.js

---
+++ angkot3.js
@@ -0,0 +1,11 @@
+var jmlAngkot =10;
+var angkotBeroperasi = 6;
+var noAngkot =1;
+
+for(var noAngkot=1; noAngkot<=jmlAngkot;noAngkot++){
+    if(noAngkot <=6){
+        console.log('Angkot No. '+noAngkot+'Angkot yang beroperasi');
+    }else{
+        console.log('Angkot No. '+noAngkot+'Angkot yang tidak beroperasi')
+    }
+}
